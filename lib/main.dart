da5bbd30a0cb770402e1863eb7c85c830c0f5360
Sam M. Vareka <84973673+Ponchoru-recur@users.noreply.github.com>
2026-05-20 10:08:28 +0800

    BIG SAVE

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,7 @@ class MyApp extends StatelessWidget {
         '/chatScreen': (context) => const ChatsScreen(),
         '/updatesTasks': (context) => const UpdatesTasksScreen(),
         '/tasks': (context) => const TaskListScreen(),
+        '/today': (context) => const TodayScreen(),
         '/pendingApproval': (context) => const PendingApprovalScreen(),
       },
     ),
